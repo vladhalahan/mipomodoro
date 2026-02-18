c2d3624694293ee77fb77865a8c7b9b0508c0a12
Vlad Halahan <vhalahan@simulmedia.com>
2026-02-18 22:24:32 +0200

    Update work/rest duration

--- constants/pomodoro.ts
+++ constants/pomodoro.ts
@@ -1,5 +1,7 @@
-export const WORK_DURATION_SEC = 1 * 60; // 40 min
-export const REST_DURATION_SEC = 0.5 * 60; // 20 min
+/** 40 min in prod (real devices), 1 min in __DEV__ (local/Expo Go) */
+export const WORK_DURATION_SEC = __DEV__ ? 1 * 60 : 40 * 60;
+/** 20 min in prod, 0.5 min in __DEV__ */
+export const REST_DURATION_SEC = __DEV__ ? 0.5 * 60 : 20 * 60;
 
 export const MIN_TASK_NAME_LENGTH = 3;
 
